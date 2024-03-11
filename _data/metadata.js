bcfe1d402d663634829c071d9fc1e762339d20a2
zazu7765 <ohmyzazu@gmail.com>
2024-03-11 11:07:16 -0400

    forgot metadata file

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "maximum recursion depth exceeded",
+	url: "https://blog.skillissue.cc/",
+	language: "en",
+	description: "I am writing about my experiences as a university student.",
+	author: {
+		name: "Daniel Ibanescu",
+		email: "zazu7765@gmail.com",
+		url: "https://skillissue.cc"
+	}
+}
